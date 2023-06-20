37293c755ef2279564f109cfbf4373dedd060372
henrique <henrique.florencio307@gmail.com>
2023-06-20 20:52:15 -0300

    resolvendo problemas de conexão com o mongodb

--- src/api/config/mongoConfig.ts
+++ src/api/config/mongoConfig.ts
@@ -6,7 +6,7 @@ dotenv.config();
 mongoose.set("strictQuery", false);
 
 const connection = mongoose.connect(
-  `mongodb+srv://${process.env.usuario}:${process.env.senha}@cluster0.dnraogv.mongodb.net/?retryWrites=true&w=majority`
+  `mongodb+srv://${process.env.usuario}:${process.env.senha}@living-challenge-cluste.chbkahh.mongodb.net/?retryWrites=true&w=majority`
 );
 
 const db = mongoose.connection;
